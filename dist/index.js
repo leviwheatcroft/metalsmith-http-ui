d3f94ee930a8e45d1cf40963acc3eb88cb11a27c
Levi Wheatcroft <levi@wheatcrofts.com.au>
2017-04-04 19:58:57 +0800

    cli args can be passed in

--- dist/index.js
+++ dist/index.js
@@ -17,23 +17,17 @@ var _child_process = require('child_process');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// import vow from 'vow'
-const dbg = (0, _debug2.default)('metalsmith-http-run'); // import {
-//   compact,
-//   extend,
-//   each,
-//   map,
-//   keys,
-//   filter,
-//   zipObject
-// } from 'lodash'
-function listen() {
+const dbg = (0, _debug2.default)('metalsmith-http-run');
+
+let params;
+function listen(..._params) {
+  params = _params;
   _http2.default.createServer(_handler).listen(3030, () => dbg('listening on 3030'));
 }
 function _handler(request, response) {
   if (request.url !== '/build') return;
   dbg('request', request);
-  const process = (0, _child_process.execFile)('node', [module.parent.filename, 'serve']);
+  const process = (0, _child_process.execFile)('node', [module.parent.filename].concat(params));
   process.on('exit', code => {
     response.write(`build exited with code: ${ code }`);
     response.end();
